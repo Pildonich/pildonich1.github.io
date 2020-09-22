2e858249828550fe83ea440c72f5144127197dcc
Pildonich <mr_divis@mail.ru>
2020-09-22 15:07:55 +0700

    4

--- js/data.js
+++ js/data.js
@@ -1,11 +1,11 @@
-const points = {
+﻿const points = {
   "type": "FeatureCollection",
   "features": [
     {
       "type": "Feature",
       "properties": {
         "description": 
-        '<strong><a href="https://sgugit.ru/" target="_blank">СГУГиТ</a></strong><p>Сибирский университет геосистем и технологий</p><img class="img-style" src="https://sgugit.ru/img/header_logo_RU.png"><a href="pano.html" target="_blank">Панорама</a>',
+        '<strong><a href="https://sgugit.ru/" target="_blank">СГУГиТ</a></strong><p>Сибирский университет геосистем и технологий</p><img class="img-style" src="https://sgugit.ru/img/header_logo_RU.png"><a href="pano.html" target="_blank">Панорама</a><p><a href="pano2.html" target="_blank">Панорама карьер</a></p>',
         "icon": "monument"
       },
       "geometry": {
