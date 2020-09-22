76cd22a4fb7cb273c23aad6a9a9009ce1eca2c9c
Pildonich <mr_divis@mail.ru>
2020-09-22 15:32:21 +0700

    5

---
+++ js/scripts.pano3.js
@@ -0,0 +1,24 @@
+var panorama, viewer, container;
+
+container = document.querySelector( '#container' );
+
+panorama = new PANOLENS.ImagePanorama( '/asset/PANO0001_2.jpg' );
+
+
+
+  // viewer.setPanorama( panorama2 );
+// } );
+
+// panorama.add( infospot );
+
+viewer = new PANOLENS.Viewer( {output: 'console', container: container});
+viewer.add( panorama );
+
+
+//viewer.addUpdateCallback(function(){
+//  
+//});
+
+
+
+
